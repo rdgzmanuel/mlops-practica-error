a049dcac5ee442d33fd26dadd2da79d46b466618
rdgzmanuel <manuel.rodriguezvillegas09@gmail.com>
2025-10-31 19:22:01 +0100

    Initial commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Cargar el modelo entrenado
 try:
-    model = joblib.load('modell.pkl')
+    model = joblib.load('model.pkl')
 except FileNotFoundError:
     print("Error: 'model.pkl' no encontrado. Por favor, asegúrate de haber ejecutado el script de entrenamiento.")
     model = None
@@ -12,23 +12,20 @@ except FileNotFoundError:
 # Inicializar la aplicación Flask
 app = Flask(__name__)
 
-@app.route('/predicts', methods=['POSST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     if model is None:
         return jsonify({'error': 'Modelo no cargado. Por favor, entrene el modelo primero.'}), 500
-
     try:
         # Obtener los datos de la petición en formato JSON
         data = request.get_json(force=True)
         features = np.array(data['features']).reshape(1, -1)
-
         # Realizar la predicción
         prediction = model.predict(features)
-
         # Devolver la predicción en formato JSON
         return jsonify({'prediction': int(prediction[0])})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=50000)
+    app.run(host='0.0.0.0', port=5000)
